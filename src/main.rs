f2b8016d6485d95d770ba33f89cf35aacff0eb53
cooscoos <m.perry@strath.ac.uk>
2022-05-29 16:55:11 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use news_reader::*;
 
 #[tokio::main]
 async fn main() {
+    // last thing to do is parse html links (multi per line) better by using remove
     let mut stories = get_top_stories();
 
     loop {
